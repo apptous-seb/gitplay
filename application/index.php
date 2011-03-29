9ea4987e2c8a42a14101024289c821d223cff507
Boris Serebrov <seb.goo@gmail.com>
2011-03-29 14:45:53 +0300

    enabled debug mode

--- application/index.php
+++ application/index.php
@@ -5,7 +5,7 @@ $yii=dirname(__FILE__).'/../packages/yii-1.1.7.r3135/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following line when in production mode
-// defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
